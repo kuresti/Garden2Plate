a7e290b9d48ec70f51fa97ea4b409e5e2f20fce9
kuresti <ure17002@byui.edu>
2024-03-30 17:11:06 -0600

    call post plant data

--- scripts/utils.js
+++ scripts/utils.js
@@ -3,6 +3,3 @@ import PlantAPIInteractions from "./APIInteractions.mjs";
 const myPlantAPIInteractions = new PlantAPIInteractions();
 myPlantAPIInteractions.searchAddEventListener();
 myPlantAPIInteractions.searchPlantData();
-myPlantAPIInteractions.postPlantData();
-
-postPlantData();
