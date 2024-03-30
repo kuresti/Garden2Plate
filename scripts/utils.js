43403e95dea74fdba0f4c50b5e324d1c2033ea22
kuresti <ure17002@byui.edu>
2024-03-30 17:04:10 -0600

    Update utils.js

--- scripts/utils.js
+++ scripts/utils.js
@@ -3,3 +3,4 @@ import PlantAPIInteractions from "./APIInteractions.mjs";
 const myPlantAPIInteractions = new PlantAPIInteractions();
 myPlantAPIInteractions.searchAddEventListener();
 myPlantAPIInteractions.searchPlantData();
+myPlantAPIInteractions.postPlantData();
